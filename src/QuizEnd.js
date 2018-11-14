a04e27c2cd8f84bdd1268ee6573ef0264bdd2325
Rahul Kumar <Rahulpatel1992>
2018-11-14 10:44:34 +0530

    QuizEnd

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class QuizEnd extends Component {
     render() { 
+        
         return (  
             <div>
                 <p>Thanks for playing!</p>
