ed460b984f92b2c4e5f53ee0d93df4925db4972b
Rahul Kumar <Rahulpatel1992>
2018-11-14 11:50:53 +0530

    QuizQuestion Changes

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import QuizQuestionButton  from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  constructor(props)
+        {
+            super(props)
+            this.state = {
+                incorrectAnswer:false
+            }
+        }
     handleClick(buttonText)
           {
-
             if(buttonText===this.props.quiz_question.answer)
             {
               this.setState({incorrectAnswer:false})
@@ -32,6 +38,7 @@ class QuizQuestion extends Component {
                   })}
               </ul>
             </section>
+            {this.state.incorrectAnswer?<p className='error'>Sorry, that's not right</p>:null}
           </main>
         );
     }
